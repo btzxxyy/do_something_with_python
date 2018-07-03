11dc0467db19c1cc18c3af86b6260bb175d2e8ce
callmexss <callmexss@126.com>
2018-07-03 23:55:10 +0800

    new commit

---
+++ map_reduce.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""""""""""""""""""""""""""""""""""""""""""""""
+"      Filename: map_reduce.py
+"
+"        Author: xss - callmexss@126.com
+"   Description: Function programming 
+"        Create: 2018-07-03 23:53:24
+"""""""""""""""""""""""""""""""""""""""""""""""
+
+iter_li = map(lambda x: x ** 2, [x for x in range(1, 4)])
+li = list(iter_li)
+print(li)
+
